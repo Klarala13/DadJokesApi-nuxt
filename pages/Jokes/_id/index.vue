44c7b92800e79ec37b1971b304f1777911a70436
Klara <sanchez.clara13@gmail.com>
2019-08-05 22:31:16 +0200

    corrected syntax

--- pages/Jokes/_id/index.vue
+++ pages/Jokes/_id/index.vue
@@ -9,24 +9,26 @@
 
 <script>
 import axios from "axios";
+
 export default {
   data(){
     return {
       joke: {}
-    }
+    };
   },
   async created(){
-    const config ={
+    const config = {
       headers: {
-        "Accept": "application/json"
+        Accept: "application/json"
       }
-    }
+    };
+    
     try { 
     const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config);
       this.joke = res.data.joke;
         console.log(this.joke);
-    }catch (err) {
-      console.error(err)
+    } catch (err) {
+      console.error(err);
     }
   },
 };
