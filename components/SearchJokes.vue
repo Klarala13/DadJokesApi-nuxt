7c45db435a95405b3f6c757bceab5aba9f0a6a8b
Klara <sanchez.clara13@gmail.com>
2019-08-05 22:31:29 +0200

    added search field

---
+++ components/SearchJokes.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="text"
+      placeholder="Search for Jokes...">
+    <input type="submit" class="btn-dark" value="Search Jokes">
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SearchJokes",
+  data() {
+    return{
+      text:""
+    };
+  },
+  methods:{
+    onSubmit(){
+      this.$emit("search-text", this.text);
+      this.text = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+  input{
+    padding: 1rem;
+    margin: 1rem;
+  }
+<style>
