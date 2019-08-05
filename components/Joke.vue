021d90d4012b871b6792f4ebc2fdb412aeada7e8
Klara <sanchez.clara13@gmail.com>
2019-08-05 19:02:54 +0200

    single joke component

---
+++ components/Joke.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="joke">
+    <p>{{ joke }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Joke",
+  props: ["joke", "id"]
+}
+</script>
+
+<style>
+<style>
