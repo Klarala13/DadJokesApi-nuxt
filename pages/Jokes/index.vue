53aad50f4eeaa2379f46c82a549a0344debb0258
Klara <sanchez.clara13@gmail.com>
2019-08-05 19:02:42 +0200

    fetch jokes

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -3,7 +3,40 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
- 
+  data(){
+    return{
+      jokes: []
+    }
+  },
+
+ async created(){
+    const config ={
+      headers: {
+        "Accept": "application/json"
+      }
+    }
+    try { 
+    const res = await axios.get("https://icanhazdadjoke.com/search", config);
+      //console.log(res.data);
+      this.jokes = res.data.results;
+    }catch (err) {
+      console.error(err)
+    }
+  },
+  head(){
+    return{
+      title:"DadJokes",
+      meta:[
+        {
+          hid:"description",
+          name:"description",
+          content: "Great dad jokes"
+        }
+      ]
+    }
+  }
 };
 </script>
