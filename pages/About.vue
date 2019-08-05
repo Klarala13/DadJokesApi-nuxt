db19741b32ac39d0be973a5e634f9663391ad1a6
Klara <sanchez.clara13@gmail.com>
2019-08-05 17:50:38 +0200

    created About page

---
+++ pages/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h1> About DadJokes</h1>
+    <p>This is an app that displays shitty dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default{
+  head(){
+    return{
+      title:"About This",
+      meta:[
+        {
+          hid:"description",
+          name:"description",
+          content: "Great dad jokes"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+h1,
+p{
+  text-align: center;
+}
+</style>
