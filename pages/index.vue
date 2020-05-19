1a08a46326352f85874a69227489a60094361426
unknown <clara.sanchez@wheelhub.es>
2020-05-19 15:40:03 +0200

    deployment changes

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+   <h2>Welcome to DadJokes</h2>
+   <p> This is a simple SPA where you can search for jokes and display them. It uses Vue, routing and Nuxt to accomplish it´s very few utilities. </p>
+  </div>
+</template>
+
+<script>
+export default {
+ head(){
+    return{
+      title:"Welcome to DadJokes",
+      meta:[
+        {
+          hid:"description",
+          name:"description",
+          content: "Great dad jokes"
+        }
+      ]
+    }
+  }
+}
+</script>
+
